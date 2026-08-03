f9557bf48795f4f056b3ba93bdab9904a48edf4a
vineet-proptiger <vineet.mishra@proptiger.com>
2026-08-03 17:49:22 +0530

    rrrr

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   webpack: (config, { dev }) => {
     if (dev) config.cache = false
     return config
